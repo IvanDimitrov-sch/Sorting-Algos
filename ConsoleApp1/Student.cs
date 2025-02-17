964f222b95863111ce18b86eafdd0f56742003be
MEGA <MEGA@gmail.com>
2025-02-17 18:38:55 +0200

    config done

---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Student
+    {
+        string name;
+        int age;
+        Student(string name, int age)
+        {
+            this.name = name;
+        }
+        
+    }
+}
