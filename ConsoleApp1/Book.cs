b2171ee9829fa14ef470c739b101be7462184a04
MEGA <MEGA@gmail.com>
2025-02-17 18:57:14 +0200

    Exercise 2 set up

---
+++ ConsoleApp1/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Book
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public Book(string author, string title)
+        {
+            this.Author = author;
+            this.Title = title;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,32 +4,39 @@
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Bubble Sort: \n");
+            BubbleSort();
+            Console.WriteLine("-------------");
+
+
+
+        }
+        static void BubbleSort()
+        {
             Student bob = new Student("Bob", 5);
             Student john = new Student("John", 7);
             Student a = new Student("a", 7);
             Student b = new Student("b", 59);
             Student c = new Student("c", 19);
             Student d = new Student("d", 29);
-            Student[] students= new Student[] { john,bob, a, c, john, b, d };
+            Student[] students = new Student[] { john, bob, a, c, john, b, d };
 
-            for (int i = 0; i < students.Length-1; i++)
+            for (int i = 0; i < students.Length - 1; i++)
             {
                 for (int j = 0; j < students.Length - i - 1; j++)
                 {
-                    if (students[j].Age > students[j+1].Age)
+                    if (students[j].Age > students[j + 1].Age)
                     {
                         (students[j], students[j + 1]) = (students[j + 1], students[j]);
                     }
                 }
             }
-            foreach(Student student in students)
+            foreach (Student student in students)
             {
                 Console.WriteLine($"{student.Name} : {student.Age}");
             }
-
-
-
-
         }
+
     }
 }
